2e362d8ae4cea28ce28a22b74516bdac791d026d
mateuszsm1smiech <75763523+mateuszsm1smiech@users.noreply.github.com>
2021-06-09 12:29:56 +0200

    xdxddxd
    poprawka

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const SERVER_NAME = "INFO";
 let users = new Map();
-let colors = new Map(); 
-app.post("/changeColor", function(req, res){
-  req.on("data", data =>{
+let colors = new Map();
+app.post("/changeColor", function (req, res) {
+  req.on("data", data => {
     parsed = JSON.parse(data);
     const color = parsed.color;
     const name = parse.name;
@@ -14,10 +14,10 @@ app.post("/changeColor", function(req, res){
   })
 });
 
-app.post("/register", function(req, res) {
-  req.on("data", data=>{
+app.post("/register", function (req, res) {
+  req.on("data", data => {
     let name = data.toString();
-    if(users.has(name) || name === SERVER_NAME){
+    if (users.has(name) || name === SERVER_NAME) {
       res.end("ERROR");
       return;
     }
@@ -27,30 +27,30 @@ app.post("/register", function(req, res) {
   });
 });
 
-app.post("/postMessage", function(req, res){
-  req.on("data", data =>{
+app.post("/postMessage", function (req, res) {
+  req.on("data", data => {
     const parsed = JSON.parse(data);
     let mes = parsed.message;
     let name = parsed.name;
     let color = colors.get(name);
-    if(mes.startsWith("/color: ")){
+    if (mes.startsWith("/color: ")) {
       color = mes.substr("/color: ".length).split(',');
-      if(color.length < 3) {
+      if (color.length < 3) {
         res.end("ERROR");
         return;
       }
       color = color.map(item => parseInt(item));
-      for(let i=0; i<3; i++){
-        if(isNaN(color[i])){
+      for (let i = 0; i < 3; i++) {
+        if (isNaN(color[i])) {
           res.end("ERROR");
           return;
         }
       }
       colors.set(name, color);
     }
-    else if(mes.startsWith("/nickname: ")){
+    else if (mes.startsWith("/nickname: ")) {
       const newName = mes.substr("/nickname: ".length);
-      if(users.has(newName)){
+      if (users.has(newName)) {
         res.end("ERROR");
         return;
       }
@@ -61,26 +61,26 @@ app.post("/postMessage", function(req, res){
       colors.delete(name);
       colors.set(newName, temp);
     }
-    users.forEach((value, key)=>{
-      if(value == null) return;
-      value.end(JSON.stringify({name: name, message: mes, color: color}));
+    users.forEach((value, key) => {
+      if (value == null) return;
+      value.end(JSON.stringify({ name: name, message: mes, color: color }));
     });
     res.end("OK");
   });
 });
 
-app.post("/getMessage", function(req, res){
+app.post("/getMessage", function (req, res) {
   let name = "";
-  req.on("data", (data)=>{
+  req.on("data", (data) => {
     name = data.toString();
     users.set(name, res);
-  }).on("close", ()=>{
+  }).on("close", () => {
     users.delete(name);
   });
 });
 
 app.use(express.static('static'));
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
   console.log(`port is listening on http://localhost:${PORT}`);
 });
